f9f54bd5e11cfd97546c798e6ac04cf4f1077d23
VapormaxGYP <vapormaxgyp@gmail.com>
2023-08-17 23:24:21 +0800

    update send Message

--- src/main/java/com/vapor/daisybot/DaisyBot.java
+++ src/main/java/com/vapor/daisybot/DaisyBot.java
@@ -1,11 +1,15 @@
 package com.vapor.daisybot;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+@Slf4j
 @Component
 public class DaisyBot extends TelegramLongPollingBot {
 
@@ -14,13 +18,29 @@ public class DaisyBot extends TelegramLongPollingBot {
 
     public DaisyBot(DefaultBotOptions botOptions) {
         super(botOptions, "6524420386:AAEIZziA-v5zn0pDCvhDjUi5rSORPn7ErcM");
+
     }
 
     @Override
     public void onUpdateReceived(Update update) {
 
         String text = update.getMessage().getText();
-        System.out.println(text);
+        log.info("Get from bot: {}", text);
+
+        sendMessage(update.getMessage().getChatId(), text);
+    }
+
+    public void sendMessage(long chatId, String response){
+
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(chatId);
+        sendMessage.setText(response);
+
+        try{
+            execute(sendMessage);
+        } catch (TelegramApiException e){
+            log.info("Send Message Error {}" , e.getMessage());
+        }
 
     }
 
